40242e7934f27dc39815c226c1bee4e931c0c0e1
BugHunter-X <askzenai@gmail.com>
2025-01-28 06:02:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+<div class="bg-gray-300 p-4">
+  <div class="flex items-center space-x-4">
+    <div class="w-16 h-16 rounded-full bg-gray-100"></div>
+    <div>
+      <p class="text-lg font-medium">User Name</p>
+      <p class="text-sm text-gray-600">@username</p>
+    </div>
+  </div>
+</div>
+```
